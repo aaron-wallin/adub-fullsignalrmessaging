6f7c2b6ef90b4918ebf384587290b2cbdc11dc96
aaron-wallin <aaron.wallin@gmail.com>
2018-08-19 17:05:31 -0700

    allow http and https in cors

--- Adub.SignalR/Adub.SignalR/Startup.cs
+++ Adub.SignalR/Adub.SignalR/Startup.cs
@@ -23,7 +23,7 @@ namespace Adub.SignalR
             {
                 builder.AllowAnyHeader()
                         .AllowAnyMethod()
-                        .WithOrigins("https://adub-signalr-webclient.apps.pcf.nonprod.cudirect.com")
+                        .WithOrigins("https://adub-signalr-webclient.apps.pcf.nonprod.cudirect.com", "http://adub-signalr-webclient.apps.pcf.nonprod.cudirect.com")
                         .AllowCredentials();
             }));
 
@@ -54,7 +54,7 @@ namespace Adub.SignalR
             {
                 builder.AllowAnyHeader()
                         .AllowAnyMethod()
-                        .WithOrigins("https://adub-signalr-webclient.apps.pcf.nonprod.cudirect.com")
+                        .WithOrigins("https://adub-signalr-webclient.apps.pcf.nonprod.cudirect.com", "http://adub-signalr-webclient.apps.pcf.nonprod.cudirect.com")
                         .AllowCredentials();
             });
 
